e1a8a47f98778c1450b56ed93dab9a6bbd4e512c
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-14 13:04:05 +0530

    made the changes

--- asci.sh
+++ asci.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-apt install cowsay -y
-cowsay -f dragon "Run for cover, I'm a DRAGON...EAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls -lrta
+echo "Script was executed"
